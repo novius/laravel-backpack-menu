de5eb65ec696be271a2698561d539d634d959076
mesa <mesa@novius.fr>
2017-10-16 10:09:19 +0200

    adds overridable function "linkableTitle" into the trait

--- src/LinkedItems.php
+++ src/LinkedItems.php
@@ -25,7 +25,7 @@ trait LinkedItems
     {
         return static::all()->mapWithKeys(function ($item) use ($prefix) {
             return [
-                implode(static::$delimiter, [$item->id, get_class($item)]) => static::linkableLabel($item->name, $prefix),
+                implode(static::$delimiter, [$item->id, get_class($item)]) => static::linkableLabel($item->linkableTitle(), $prefix),
             ];
         })->toArray();
     }
@@ -65,6 +65,11 @@ trait LinkedItems
         return url($this->slug);
     }
 
+    /**
+     * Returns the label for the item. Title by default.
+     * Optionally overridable within items having no title (for instance name)
+     * @return string
+     */
     public function linkableTitle()
     {
         return $this->title;
