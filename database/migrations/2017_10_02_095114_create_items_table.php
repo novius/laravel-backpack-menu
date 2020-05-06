022152d1eb3c4685b4c998995826348de92918e7
gaubert <gaubert@novius.fr>
2020-05-06 17:11:15 +0200

    update for laravel 5.7

--- database/migrations/2017_10_02_095114_create_items_table.php
+++ database/migrations/2017_10_02_095114_create_items_table.php
@@ -14,7 +14,7 @@ class CreateItemsTable extends Migration
     public function up()
     {
         Schema::create('novius_menu_items', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
             $table->string('name');
             $table->timestamps();
         });
